eeb431b03ee09d1200f6de959325e8d606dfd85b
omerhagag2511 <omerhagag2511@gmail.com>
2026-01-11 19:58:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/project06/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.project06;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.text);
+    }
+
+    public void go(View view) {
+        text.setText("Oh, yea, I’ve been clicked!");
+    }
+}
